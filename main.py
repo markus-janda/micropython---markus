5d64e4512cdfd24dd850376dc5eb272cca49c4e3
markus-janda <80055070+markus-janda@users.noreply.github.com>
2021-03-24 13:42:34 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,9 @@
+from microbit import *
+
+answer = 42 
+hello_message = "Hi there!"
+    
+basic.show_string(hello_message)
+hello_message = "Yo!"
+basic.show_string(hello_message)
+
---
+++ main.ts
@@ -0,0 +1,5 @@
+let answer = 42
+let hello_message = "Hi there!"
+basic.showString(hello_message)
+hello_message = "Yo!"
+basic.showString(hello_message)
